71a61ff757f1622880eafeb77b0219eab17c7c71
Better <rsm5909@gmail.com>
2020-01-09 01:59:01 -0500

    updated

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ class FercSpider:
 
     @staticmethod
     def upload_dropbox(links, r, **kwargs):
-        access_token = 'x1'
+        access_token = 'x12'
         today = date.today().strftime("%m/%d/%Y").replace('/', '-')
         dbx = dropbox.Dropbox(access_token)
         path = '/test/{}/'.format(today)
